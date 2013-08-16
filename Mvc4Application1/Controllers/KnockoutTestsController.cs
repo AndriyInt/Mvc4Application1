961e123e1f787297901eec1ca4b4f9da669091c8
Andriy Fetsyuk <AndreusAnd@gmail.com>
2013-08-16 21:49:20 +0300

    added knockout sample

---
+++ Mvc4Application1/Controllers/KnockoutTestsController.cs
@@ -0,0 +1,21 @@
+﻿namespace Mvc4Application1.Controllers
+{
+    using System.Web.Mvc;
+
+    public class KnockoutTestsController : Controller
+    {
+        //
+        // GET: /KnockoutTests/
+
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
+        public ActionResult Step2()
+        {
+            return this.View();
+        }
+
+    }
+}
