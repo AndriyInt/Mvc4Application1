90dbe45d824e24ba1d92c7d74a88ec5625b7b2d6
Andriy Fetsyuk <AndreusAnd@gmail.com>
2013-09-07 22:29:30 +0300

    Now version is visible on Admin controller

---
+++ Mvc4Application1/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc4Application1.Controllers
+{
+    using System.Globalization;
+    using System.Reflection;
+
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+
+        public ActionResult Index()
+        {
+            var informationalVersionAttribute =
+                (AssemblyInformationalVersionAttribute)
+                Assembly.GetExecutingAssembly().GetCustomAttribute(typeof(AssemblyInformationalVersionAttribute));
+            this.ViewBag.Version = informationalVersionAttribute.InformationalVersion;
+            return this.View();
+        }
+
+    }
+}
