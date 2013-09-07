863e8f43ccdacef8dfb6660a6c16e16f601c8b90
Andriy Fetsyuk <AndreusAnd@gmail.com>
2013-09-08 00:37:12 +0300

    Added showing logs logic to Admin controller

--- Mvc4Application1/Controllers/AdminController.cs
+++ Mvc4Application1/Controllers/AdminController.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace Mvc4Application1.Controllers
+﻿namespace Mvc4Application1.Controllers
 {
+    using System;
+    using System.Collections.Generic;
     using System.Globalization;
+    using System.Linq;
     using System.Reflection;
+    using System.Web;
+    using System.Web.Mvc;
 
     public class AdminController : Controller
     {
-        //
-        // GET: /Admin/
+        private const string LogDir = "~/Logs/";
 
         [Authorize(Roles = "Admin, Super User")]
         public ActionResult Index()
@@ -25,9 +23,61 @@ namespace Mvc4Application1.Controllers
         }
 
         [Authorize(Roles = "Admin, Super User")]
+        public ActionResult ShowLogs(string path)
+        {
+            //path = "elmah";
+            var fullPath = Server.MapPath(string.Format("{0}{1}/", LogDir, path));
+            var fullPathLength = fullPath.Length;
+
+            string parentDirPath = null;
+            if (path != null)
+            {
+                var slashIdx = path.LastIndexOf('/');
+                parentDirPath = slashIdx > -1 ? path.Substring(0, slashIdx) : string.Empty;
+            }
+
+            var subdirs = System.IO.Directory.EnumerateDirectories(fullPath).Select(fname => fname.Substring(fullPathLength));
+            var files = System.IO.Directory.EnumerateFiles(fullPath).Select(fname => fname.Substring(fullPathLength));
+
+            return this.View(new DirectoryListing.DirectoryListing
+                                 {
+                                     Path = path,
+                                     ParentDirPath = parentDirPath,
+                                     Subirectories = subdirs,
+                                     Files = files
+                                 });
+        }
+
+        [Authorize(Roles = "Admin, Super User")]
         public void DownloadLog()
         {
             this.Response.WriteFile(Server.MapPath("~/Logs/2013.09.07.log.resources"));
+        }
+        
+        /// <summary>
+        /// Downloads file
+        /// </summary>
+        /// <param name="relFname">relative to LogDir directory</param>
+        [Authorize(Roles = "Admin, Super User")]
+        public void DownloadFile(string relFname)
+        {
+            ////this.Response.WriteFile(Server.MapPath("~/Logs/2013.09.07.log.resources"));
+
+            var fullFilePath = Server.MapPath(string.Format("{0}/{1}", LogDir, relFname));
+            var file = new System.IO.FileInfo(fullFilePath);
+            
+            Response.Clear();
+            Response.ClearHeaders();
+            Response.ClearContent();
+            Response.AddHeader("Content-Disposition", "attachment; filename=" + file.Name);
+            Response.AddHeader("Content-Length", file.Length.ToString(CultureInfo.InvariantCulture));
+            Response.ContentType = "text/plain";
+
+            Response.Flush();
+
+            Response.TransmitFile(file.FullName);
+
+            Response.End();
         }
     }
 }
---
+++ Mvc4Application1/DirectoryListing/DirectoryListing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mvc4Application1.DirectoryListing
+{
+    public class DirectoryListing
+    {
+        public string Path { get; set; }
+
+        public string ParentDirPath { get; set; }
+
+        public IEnumerable<string> Subirectories { get; set; }
+
+        public IEnumerable<string> Files { get; set; }
+    }
+}
