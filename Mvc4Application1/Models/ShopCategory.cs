eb740d69c5843221454c1cce05979310572fcc15
Andriy Fetsyuk <AndreusAnd@gmail.com>
2013-09-15 01:20:33 +0300

    Category display improvements

--- Mvc4Application1/Models/ShopCategory.cs
+++ Mvc4Application1/Models/ShopCategory.cs
@@ -3,6 +3,7 @@
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
 
     public class ShopCategory
     {
@@ -19,8 +20,23 @@
         
         public ICollection<ShopCategory> Subcategories { get; set; }
         
+        [Display(Name = "Parent Category")]
         public ShopCategory ParentCategory { get; set; }
-        
+
+        public string Path
+        {
+            get
+            {
+                var hierarchy = new LinkedList<ShopCategory>();
+                for (var t = this; t != null; t = t.ParentCategory)
+                {
+                    hierarchy.AddFirst(t);
+                }
+
+                return string.Join("/", hierarchy.Select(c => c.Name));
+            }
+        }
+
         public ICollection<ShopProduct> Products { get; set; }
 
         ////public ShopCategory()
